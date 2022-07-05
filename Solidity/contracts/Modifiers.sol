8dafe101f9ea27d2c5a51c0a2216b30394990b7e
Ihtisham <ahsaanullah174@gmail.com>
2022-07-05 13:08:51 +0500

    Updating Modifiers

--- Solidity/contracts/Modifiers.sol
+++ Solidity/contracts/Modifiers.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+
+
 contract MyModifier{
     uint256 public myNum = 0;
     bool public paused = false;
@@ -9,8 +11,11 @@ contract MyModifier{
     constructor (){
         owner = address(msg.sender);
     }
-    modifier isNotPaused (){
-        require(paused == false, "Contract is paused");
+    modifier isNotPaused (bool _bypass){
+        if(_bypass == true){
+             require(paused == false, "Contract is paused");
+        }
+       
         _;
     }
     modifier onlyOwner(){
@@ -21,10 +26,10 @@ contract MyModifier{
     function setPause()public{
         paused = true;
     }
-    function addToNum() public isNotPaused onlyOwner{
+    function addToNum() public isNotPaused(true) onlyOwner{
         myNum++;
     }
-    function subToNum() public isNotPaused onlyOwner{
+    function subToNum() public isNotPaused(false) onlyOwner{
         myNum--;
     }
 }
